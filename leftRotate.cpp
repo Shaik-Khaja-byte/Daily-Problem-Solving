757d8491583146988262bea6f763e3f692615b07
Shaik-Khaja-Byte <khaja4756@gmail.com>
2026-08-01 23:39:12 +0530

    Added: rotate-left array elements by k

---
+++ leftRotate.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void leftRotate(vector<int>& vec, int n, int k){
+    // to rotate array left by k elements
+    k = k % n; // first simplify k
+    vector<int> temp(k); // create an array of size k - it will holdd the starting k values
+    
+    for(int i = 0; i < k; i++){ // put the starting k values of vec into temp 
+        temp[i] = vec[i];
+    }
+
+    for(int i = 0; i < n - k; i++){ // shift the array elements left
+        vec[i] = vec[i + k];
+    }
+
+    for(int i = n - k; i < n; i++){ // transfer the elements back to vec
+        vec[i] = temp[i - (n - k)];
+    }
+}
+
+int main(){
+    vector<int> vec = {1, 2, 3, 4, 5, 6, 7};
+    int n = vec.size();
+    cout << "Enter the value of k: ";
+    int k;
+    cin >> k;
+
+    leftRotate(vec, n, k);
+
+    for (auto it : vec){
+        cout << it << " ";
+    }
+
+    return 0;
+}
