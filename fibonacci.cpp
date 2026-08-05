6c12ce22743794ae4dd7b491361180c15309430c
Shaik Khaja <khaja4756@gmail.com>
2026-08-05 21:38:57 +0530

    Update fibonacci.cpp
    Added: Comments for the fibonacci

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -8,13 +8,13 @@ int main() {
 
     int a = 0, b = 1, c;
 
-    if(n == 0) c = 0;
-    else if(n == 1) c = 1;
+    if(n == 0) c = 0; //base case 1
+    else if(n == 1) c = 1; //base case 2
     else {
         for(int i = 2; i <= n; i++) {
-            c = a + b;
-            a = b;
-            b = c;
+            c = a + b; // sum of the previous two elements 
+            a = b; // update a
+            b = c; // update b
         }
     }
     cout << n << "th Fibonacci = " << c;
