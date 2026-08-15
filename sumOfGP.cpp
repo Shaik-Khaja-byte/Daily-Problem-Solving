e16663a0798af27464068a5421143f9d18e1bef0
Shaik Khaja <khaja4756@gmail.com>
2026-08-15 22:11:31 +0530

    Update sumOfGP.cpp

--- sumOfGP.cpp
+++ sumOfGP.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 int main() {
     int a, r, n;
-    cin >> a >> r >> n;
+    cin >> a >> r >> n; // first term, common diff, no. of elements in the series 
 
     int sum;
     
     if(r == 1){
         sum = n * a;
     } else {
+        // formula to calculate GP
         sum = a * (pow(r, n) - 1) / (r - 1);
     }
 
