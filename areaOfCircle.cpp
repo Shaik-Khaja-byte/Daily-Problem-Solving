49d74d56994c529d611f49760c550e8571e9b0b5
Shaik-Khaja-Byte <khaja4756@gmail.com>
2026-09-15 23:54:39 +0530

    Added: area of a circle

---
+++ areaOfCircle.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define PI 3.142
+using namespace std;
+
+
+int main() {
+    int radius{};
+    cin >> radius;
+
+    cout << "Area of a circle = " << PI * radius * radius;
+    return 0;
+}
